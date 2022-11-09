2ef8292a9e529fd68422d9e6ca94c296dd14374c
MansiSrivastava6 <72976787+MansiSrivastava6@users.noreply.github.com>
2022-11-09 20:00:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import helper
+import pickle
+
+rf_final = pickle.load(open('rf_final.pkl','rb'))
+
+st.header('Duplicate Question Pairs')
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query = helper.query_point_creator(q1,q2)
+    result = rf_final.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
